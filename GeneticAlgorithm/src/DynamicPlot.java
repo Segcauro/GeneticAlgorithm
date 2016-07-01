29b0003d6e9a2c58b011d7f386535f3ac15c23fb
Segcauro <nic-1995@hotmail.de>
2016-07-01 12:08:48 +0200

    Java XY-Plotting hinzugefügt

---
+++ GeneticAlgorithm/src/DynamicPlot.java
@@ -0,0 +1,94 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.Millisecond;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+/**
+ * A demonstration application showing a time series chart where you can dynamically add
+ * (random) data by clicking on a button.
+ *
+ */
+public class DynamicPlot extends ApplicationFrame{
+
+    /** The time series data. */
+    private XYSeries series;
+
+    /** The most recent value added. */
+    private double lastValue = 100.0;
+
+    public DynamicPlot(final String title) {
+
+        super(title);
+        this.series = new XYSeries("Fittest of Generation", true, true);
+        final XYSeriesCollection dataset = new XYSeriesCollection(this.series);
+        final JFreeChart chart = createChart(dataset);
+
+        final ChartPanel chartPanel = new ChartPanel(chart);
+
+        final JPanel content = new JPanel(new BorderLayout());
+        content.add(chartPanel);
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+        setContentPane(content);
+
+    }
+
+    private JFreeChart createChart(final XYDataset dataset) {
+        final JFreeChart result = ChartFactory.createXYLineChart(
+            "Genetic Algorithm", 
+            "Generation", 
+            "Fitness",
+            dataset,
+            PlotOrientation.VERTICAL,
+            true,true,false
+        );
+        final XYPlot plot = result.getXYPlot();
+        ValueAxis axis = plot.getDomainAxis();
+        axis.setAutoRange(true);
+        axis = plot.getRangeAxis(); 
+        return result;
+    }
+	
+    public void plot(int gerneration){
+    	 final double factor = 0.90 + 0.2 * Math.random();
+         this.lastValue = this.lastValue * factor;
+         final Millisecond now = new Millisecond();
+         System.out.println("Now = " + now.toString());
+         this.series.add(gerneration, this.lastValue);
+    }
+    
+    /**
+     * Starting point for the demonstration application.
+     *
+     * @param args  ignored.
+     */
+    
+    
+    /*
+    public static void main(final String[] args) {
+
+        final DynamicPlot demo = new DynamicPlot("Dynamic Data Demo");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+
+    }
+    */
+
+}
--- GeneticAlgorithm/src/GeneticAlgorithm.java
+++ GeneticAlgorithm/src/GeneticAlgorithm.java
@@ -1,8 +1,13 @@
+
+import org.jfree.ui.RefineryUtilities;
+
 import Framework.Settings;
 
 public class GeneticAlgorithm {
 	
+	private DynamicPlot demo;
 	private Settings Settings;
+	private int Genertation = 0;
 	private DNAMathFunction[] Population = new DNAMathFunction[50];
 	
 	public GeneticAlgorithm (Settings settings){
@@ -10,31 +15,48 @@ public class GeneticAlgorithm {
 	}
 	
 	public void startAlgorithm() throws InstantiationException, IllegalAccessException{
-		initialPopulation();
+		startPlotting();
+		for(int i = 0; i < 1000; i++){
+			initialPopulation();
+			printPopulation();
+			demo.plot(Genertation);
+			Genertation += 1;
+		}
+	}
+	
+	private void startPlotting(){
+        demo = new DynamicPlot("Dynamic Data Demo");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
 	}
 	
 	private void initialPopulation(){
-		
 		for(int i = 0; i < 50; i++){
 			DNAMathFunction individuum = new DNAMathFunction();
 			individuum.setRandomDNA();
 			Population[i] = individuum;
 		}
-		
+	}
+	//Helpermethode for Developer Testing
+	private void printPopulation(){
 		for(int i = 0; i < Population.length; i++){
 			Population[i].printDNA();
 			System.out.print(" --> ");
 			System.out.println(Population[i].getFitness());
 		}
 	}
-	
 	private DNAMathFunction selectFittest(){
 	
 		DNAMathFunction[] Fittest = new DNAMathFunction[10];
-		
-		
+			
 		return null;
-		
 	}
 	
+	private void crossOver(){
+		
+	}
+	private void mutatePopulation(){
+		
+	}
 }
