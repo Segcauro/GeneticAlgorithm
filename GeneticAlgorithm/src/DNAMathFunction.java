1d23f561a967f5acd6a3a0096db8c35386bc7c42
SE-GR <sebastian-griesbach@freenet.de>
2016-07-09 20:50:52 +0200

    Buchstaben sortieren
    Das Problem besteht nun darin eine Reihe von buchstaben zu sortieren. Es
    ist allerdings keine wirkliche sortiertung da dies vorraus setzten
    würde, dass jedes Element nur einmal vorhanden ist. Dies würde die
    Rekombination ungemein erschweren.
    Zur Zeit ist das Programm nicht ausführbar, da es ein problem mit dem
    Set gibt und darin das fittets Individuum zu ziehen, welches ich nicht
    verstehe.

--- GeneticAlgorithm/src/DNAMathFunction.java
+++ GeneticAlgorithm/src/DNAMathFunction.java
@@ -1,12 +1,13 @@
+import java.util.List;
 import java.util.Random;
 
 import Framework.IDNA;
 
 public class DNAMathFunction implements IDNA, Comparable, java.io.Serializable{
 
-	private String[] DNA = new String[11];
-	private int DNALength = 11;
-	private String[] DNAValues = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"};
+	private String[] DNA = new String[26];
+	private int DNALength = DNA.length;
+	private String[] DNAValues = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 	private int numberOfDNAValues = DNAValues.length - 1;
 	
 	public void printDNA(){
@@ -17,31 +18,30 @@ public class DNAMathFunction implements IDNA, Comparable, java.io.Serializable{
 	}
 	
 	public double getFitness() {
-		int a = Integer.parseInt(DNA[0]);
-		int b = Integer.parseInt(DNA[1]);
-		int c = Integer.parseInt(DNA[2]);
-		int d = Integer.parseInt(DNA[3]);
-		int e = Integer.parseInt(DNA[4]);
-		int f = Integer.parseInt(DNA[5]);
-		int g = Integer.parseInt(DNA[6]);
-		int h = Integer.parseInt(DNA[7]);
-		int i = Integer.parseInt(DNA[8]);
-		int j = Integer.parseInt(DNA[9]);
-		int k = Integer.parseInt(DNA[10]);
-		
-		//int value1 = (c * (a-7) + 9) *  (f+3)+i*4 - (j-5);
-		//int value1 = (int)( Math.pow((a-2), 2) + Math.pow((b-3),2) - 1);
-		int value1 = 2;
-		int fitness = (int)( Math.pow((a-4), 2) + Math.pow((b-2),2) - 4);
-		//int value2 = (4* (b-d)+e)* (6+g)+1*h - (8-k);
-		//int value2 = a+b+c+d+e+f+g+h+i+j+k;
-		//(x-2)^2+(y-3)^2-1 = (x-4)^2+(y-2)^2-4
-		
-		return fitness * Math.cos(a - d) * Math.sin(b - k) * Math.sin(j - g);
+		int fitness = 0;
+		for(int i = 0; i < DNALength; i++)
+		{
+			if(DNA[i] == DNAValues[i])
+			{
+				fitness++;
+			}
+		}
+
+		return fitness;
 	}
 
 	public void setRandomDNA() {
 		Random random = new Random();
+		
+		for(int i = 0; i < numberOfDNAValues; i++)
+		{
+			Random index = new Random();
+			int min = 0;
+			int max = numberOfDNAValues;
+			
+			DNA[i] = DNAValues[index.nextInt(max - min + 1) + min];
+		}
+		
 		int min = 1;
 		int max = numberOfDNAValues;
 		
@@ -55,8 +55,7 @@ public class DNAMathFunction implements IDNA, Comparable, java.io.Serializable{
 		int min = 0;
 		int max = numberOfDNAValues;
 		
-		String newValue = DNAValues[random.nextInt(max - min + 1) + min];
-		DNA[Index] = newValue;
+		DNA[Index] = DNAValues[random.nextInt(max - min + 1) + min];
 	}
 	
 	public void setDNA(String[] dna) {
